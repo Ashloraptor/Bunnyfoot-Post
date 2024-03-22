b0950d377b867acf75550a51fcb3e58cfd2d1cf1
Ash <ashloraptor@gmail.com>
2024-03-22 09:54:16 -0400

    Created User schema.

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,1 +1,23 @@
 const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    deleteUser,
+    addThought,
+    removeThought,
+} = require('../../controllers/userController');
+
+// /api/user
+router.route('/').get(getUsers).post(createUser);
+
+// /api/user/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+// /api/user/:userId/thought
+router.route('/:userId/thought').post(addThought);
+
+// /api/user/:studentId/thought/:thoughtId
+router.route(':studentId/thought/:thoughtId').delete(removeThought);
+
+module.exports = router;
