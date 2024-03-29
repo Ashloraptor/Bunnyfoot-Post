345505abdeeba986c0ee6590d6a1120ba6c50f9a
Ash <ashloraptor@gmail.com>
2024-03-29 18:01:21 -0400

    Revised README file, included link to demo video.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Bunnyfoot-Post
-AS A social media startup I WANT an API for my social network that uses a NoSQL database SO THAT my website can handle large amounts of unstructured data
+Welcome to Bunnyfoot-Post, a fictitious server for a social media app that connects various users. Make friends, post thoughts, and share reactions, and manage it all behind the scenes! Bunnyfoot-Post allows you to track friendcount and users with associated thoughts, and the reactions associated with those thoughts.
+
+![image](https://github.com/Ashloraptor/Bunnyfoot-Post/assets/148083732/0e9395bd-6bb5-4293-9171-08aa633fb9fc)
+
+
+
+[Demo Video](https://drive.google.com/file/d/15rByjqyKtyHeOw2i6kUZA99-hGm0Ni3Z/view)
--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -29,9 +29,9 @@ module.exports = {
             // const thought = await Thought.findOne({ _id: req.params.thoughtId })
             //     .populate('users');
             const thought = await Thought.findOneAndUpdate(
-                {_id: req.params.userId},
-                { $addToSet: { thoughts: req.params.thoughtId}}
-                // { runValidators: true, new: true }
+                { _id: req.params.thoughtId },
+                // { $addToSet: { thoughts: req.params.thoughtId}},
+                { runValidators: true, new: true }
 
             )
 
@@ -46,26 +46,26 @@ module.exports = {
     },
 
     // Create thought
-    // Associate it with a user
     async createThought(req, res) {
-        const {thoughtText, username} = req.body;
+        const { thoughtText, username } = req.body;
         try {
             const thought = await Thought.create(
-                {thoughtText, username},
+                { thoughtText, username },
                 // { _id: req.params.userId},
                 // { $addToSet: { thoughts: req.body}}
-                );
-                await User.findOneAndUpdate(
-                    {username},
-                    {$push: {thoughts: thought._id}},
-                    {new: true}
-                );
+            );
+            await User.findOneAndUpdate(
+                { username },
+                { $push: { thoughts: thought._id } },
+                { new: true }
+            );
             res.json(thought);
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);
         }
     },
+
     // Delete a thought
     async deleteThought(req, res) {
         try {
@@ -76,7 +76,7 @@ module.exports = {
             }
 
             await User.deleteMany({ _id: { $in: thought.users } });
-            res.json({message:'No thoughts; head empty'});
+            res.json({ message: 'No thoughts; head empty' });
         } catch (err) {
             res.status(500).json(err);
         }
@@ -100,62 +100,63 @@ module.exports = {
         }
     },
 
-        //Add a reaction to a ~user~ thought
-        async addReaction(req, res) {
-            console.log('You are adding a reaction');
-            // console.log(req.body);
-            const { thoughtId } = req.params;
-            const { reactionBody, username } = req.body;
-    
-            try {
-                // const thought = await Thought.findOneAndUpdate(
-                //     { _id: req.params.userId },
-                //     { $addToSet: { reactions: req.body } },
-                //     { runValidators: true, new: true }
-                // );
-                const thought = await Thought.findById(thoughtId);
-                if (!thought) {
-                  return res.status(404).json({ message: "Thought not found" });
-                }
-          
-                thought.reactions.push({ reactionBody, username });
-                await thought.save(); 
-    
-                if (!thought) {
-                    return res
-                        .status(404)
-                        .json({ message: 'Bunnyfoot lost that trail of thought' });
-                }
-    
-                res.json(thought);
-            } catch (err) {
-                res.status(500).json(err);
-            }
-        },
-    
-        // Remove reaction from a ~user~ thought
-        async removeReaction(req, res) {
-            const { thoughtId, reactionId } = req.params;
-            try {
-                // const thought = await Thought.findOneAndUpdate(
-   
-                //     { _id: req.params.userId },
-                //     { $pull: { reaction: { reactionId: req.params.reactionId } } },
-                //     { runValidators: true, new: true }
-                // );
-                const thought = await Thought.findById(thoughtId);
-                if (!thought) {
-                    return res
-                        .status(404)
-                        .json({ message: 'Bunnyfoot lost that trail of thought' });
-                }
-    
-                thought.reactions.pull(reactionId);
-                await thought.save();
+    //Add a reaction to a ~user~ thought
+    async addReaction(req, res) {
+        console.log('You are adding a reaction');
+        // console.log(req.body);
+        const { thoughtId } = req.params;
+        const { reactionBody, username } = req.body;
 
-                res.json({message: "Reaction deleted!"});
-            } catch (err) {
-                res.status(500).json(err);
+        try {
+            // const thought = await Thought.findOneAndUpdate(
+            //     { _id: req.params.userId },
+            //     { $addToSet: { reactions: req.body } },
+            //     { runValidators: true, new: true }
+            // );
+            const thought = await Thought.findById(thoughtId);
+            if (!thought) {
+                return res.status(404).json({ message: "Thought not found" });
             }
-        },
+
+            thought.reactions.push({ reactionBody, username });
+            await thought.save();
+
+            if (!thought) {
+                return res
+                    .status(404)
+                    .json({ message: 'Bunnyfoot lost that trail of thought' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // Remove reaction from a thought
+    async removeReaction(req, res) {
+        const { thoughtId, reactionId } = req.params;
+        try {
+            const thought = await Thought.findOneAndUpdate(
+
+                { _id: req.params.thoughtId },
+                // { _id: req.params.userId },
+                { $pull: { reaction: { reactionId: req.params.reactionId } } },
+                // { runValidators: true, new: true }
+            );
+            // const thought = await Thought.findById(thoughtId);
+            if (!thought) {
+                return res
+                    .status(404)
+                    .json({ message: 'Bunnyfoot lost that trail of thought' });
+            }
+
+            // thought.reactions.pull(reactionId);
+            // await thought.save();
+
+            res.json({ message: "Reaction deleted!" });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 };
